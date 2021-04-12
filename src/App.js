55c1080647350f54e1e0538ed1fa00e38d7613e5
IrmIris33 <irmirisortizortiz@outlook.com>
2021-04-12 09:53:03 -0400

    Started Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
 
 function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    quotes: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://ron-swanson-quotes.herokuapp.com/v2/quotes/20`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((quotes) => {
+        setAppState({ loading: false, quotes: quotes });
+      });
+  }, [setAppState]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} quotes={appState.quotes} />
+      </div>
     </div>
   );
 }
-
 export default App;
